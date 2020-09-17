4b84d13f66c8ada278a695ecdfa2d630af774dfa
unknown <you@example.com>
2020-09-17 11:39:59 +0300

    post_type#2

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -4,7 +4,15 @@ function university_files() {
 
     wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_script('main_university_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+
+    if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+        wp_enqueue_script('main_university_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('main_university_js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+    }
+
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
@@ -17,3 +25,4 @@ function university_features() {
     add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
+
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -10,7 +10,7 @@ while(have_posts()) {
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
-                <p>Dont forget to replace me</p>
+                <p>Dont forget to replace me: Page</p>
             </div>
         </div>
     </div>
